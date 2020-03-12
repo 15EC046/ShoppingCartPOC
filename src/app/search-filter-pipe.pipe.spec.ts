3e0e22b9fefe9175366a4b1f9ab6a0da1af0475f
SOPHIA AGNUS H <hendrysophia003@gmail.com>
2020-03-12 11:47:01 +0530

    changed

--- src/app/search-filter-pipe.pipe.spec.ts
+++ src/app/search-filter-pipe.pipe.spec.ts
@@ -1,8 +1,8 @@
-import { SearchFilterPipePipe } from './search-filter-pipe.pipe';
+import { SearchPipe } from './search-filter-pipe.pipe';
 
 describe('SearchFilterPipePipe', () => {
   it('create an instance', () => {
-    const pipe = new SearchFilterPipePipe();
+    const pipe = new SearchPipe();
     expect(pipe).toBeTruthy();
   });
 });
